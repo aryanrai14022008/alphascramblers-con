7ca69546627f5f414c47c24d69a864ee2b5bd5aa
PrakharTagra <prakhartagra16@gmail.com>
2025-07-01 05:18:49 +0530

    i have done it

--- alpha.js
+++ alpha.js
@@ -1,1 +1,15 @@
-let menu = document.getElementsByClassName(".navcon")
+let bars = document.querySelector(".navcon");
+let menu = document.querySelector(".navcon-ele");
+let currstat="unvis";
+bars.addEventListener("click",()=>{
+if(currstat=="unvis"){
+    currstat="vis"
+    menu.classList.remove("navcon-ele");
+    menu.classList.add("navcon-ele-new");
+}
+else{
+    currstat="unvis"
+    menu.classList.remove("navcon-ele-new");
+    menu.classList.add("navcon-ele");
+}
+})
