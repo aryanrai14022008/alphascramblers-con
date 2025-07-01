00673e27ae791fb688bcafda8e1cfb2c00379ea6
PrakharTagra <prakhartagra16@gmail.com>
2025-07-02 00:42:54 +0530

    banner

--- alpha.js
+++ alpha.js
@@ -16,17 +16,17 @@ else{
 let banner = document.querySelector(".banner");
 let bannerno=1;
 function newBanner(){
-    if(bannerno<3){
-        banner.style.transform=`translateX(-${bannerno*100}%)`
+    if(bannerno<5){
+        banner.style.transform=`translateX(-${bannerno*100}vw)`
         bannerno++;}
-    else if(bannerno==3){
+    else if(bannerno==5){
         setTimeout(()=>{
             banner.style.transition="none";
             banner.style.transform="translateX(0%)";
         },0)
         setTimeout(()=>{
-            banner.style.transition="transform 10s ease-in-out";
+            banner.style.transition="transform 1.5s ease-in-out";
             bannerno=1;
         },50)}
 }
-setInterval(newBanner,4000);
+setInterval(newBanner,3000);
