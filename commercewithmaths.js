da458982a312ba979cdbc053a6868751694a2715
aryan rai <aryanrai1402@gmail.com>
2025-07-22 00:50:56 +0530

    d

---
+++ commercewithmaths.js
@@ -0,0 +1,14 @@
+let bars = document.querySelector(".navcon");
+let menu = document.querySelector(".navcon-ele");
+let currstat="unvis";
+bars.addEventListener("click",()=>{
+if(currstat=="unvis"){
+    currstat="vis"
+    menu.classList.remove("navcon-ele");
+    menu.classList.add("navcon-ele-new");
+}
+else{
+    currstat="unvis"
+    menu.classList.remove("navcon-ele-new");
+    menu.classList.add("navcon-ele");
+}})
