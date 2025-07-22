9e00c29b99dfe074eb8f1c37acf4dff4949548df
PrakharTagra <prakhartagra16@gmail.com>
2025-07-22 18:23:56 +0530

    Preet final correction

--- helpandsupport/helpandsupport.js
+++ helpandsupport/helpandsupport.js
@@ -28,3 +28,23 @@ regbtn.addEventListener("click",()=>{
     overlay.classList.add("overlay1");
     document.body.style.overflow="hidden";
 })
+for(let i=1;i<=10;i++){
+    let button = document.querySelector(`.b${i}`);
+    let answer = document.querySelector(`.a${i}`);
+    let answernew = document.querySelector(`.a${i}n`)
+
+    button.addEventListener("click",()=>{
+    if(currstat=="unvis"){
+        currstat="vis"
+        answer.classList.remove(`a${i}`);
+        answer.classList.add(`a${i}n`)
+        button.innerHTML="-"
+    }
+    else{
+        currstat="unvis"
+        answer.classList.remove(`a${i}n`);
+        answer.classList.add(`a${i}`)
+        button.innerHTML="+"
+    }
+})
+}
