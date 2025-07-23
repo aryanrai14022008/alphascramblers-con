a669edf122f64370e326c264740db4bcde34b3b9
PrakharTagra <prakhartagra16@gmail.com>
2025-07-23 14:55:46 +0530

    short

--- homepage/alpha.js
+++ homepage/alpha.js
@@ -1,22 +1,3 @@
-let header = document.querySelector("header");
-let main = document.querySelector("main");
-let footer = document.querySelector("footer");
-let overlay=document.querySelector(".overlay");
-let bars = document.querySelector(".navcon");
-let menu = document.querySelector(".navcon-ele");
-let currstat="unvis";
-bars.addEventListener("click",()=>{
-if(currstat=="unvis"){
-    currstat="vis"
-    menu.classList.remove("navcon-ele");
-    menu.classList.add("navcon-ele-new");
-}
-else{
-    currstat="unvis"
-    menu.classList.remove("navcon-ele-new");
-    menu.classList.add("navcon-ele");
-}
-})
 let banner = document.querySelector(".banner");
 let bannerno=1;
 function newBanner(){
@@ -34,14 +15,3 @@ function newBanner(){
         },50)}
 }
 setInterval(newBanner,5000);
-overlay.style.height=`${document.documentElement.scrollHeight}px`
-let register = document.querySelector(".register");
-let regbtn = document.querySelector(".login");
-regbtn.addEventListener("click",()=>{
-    register.classList.remove("regdis");
-    header.style.opacity="0.3";
-    main.style.opacity="0.3";
-    footer.style.opacity="0.3";
-    overlay.classList.add("overlay1");
-    document.body.style.overflow="hidden";
-})
