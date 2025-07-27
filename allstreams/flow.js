15eb312e7ce3db8d15b2aa0fc22f4a06af94c864
PrakharTagra <prakhartagra16@gmail.com>
2025-07-27 10:58:56 +0530

    linkage

--- allstreams/flow.js
+++
@@ -1,8 +0,0 @@
-const flow = document.getElementById("ss1");
-flow.addEventListener("load",()=>{
-    const svgdoc= flow.contentDocument;
-    const b1 = svgdoc.getElementById("b1");
-    b1.addEventListener("click",()=>{
-        console.log("b1")
-    })
-})
